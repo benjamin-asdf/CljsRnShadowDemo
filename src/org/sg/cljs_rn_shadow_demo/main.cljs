85460f6a5c316b0da017c74d11754e78d492ab6e
Benj <Benjamin.Schwerdtner@gmail.com>
2022-05-26 12:52:11 +0200

    Add hello world

--- src/org/sg/cljs_rn_shadow_demo/main.cljs
+++ src/org/sg/cljs_rn_shadow_demo/main.cljs
@@ -1,8 +1,28 @@
 (ns org.sg.cljs-rn-shadow-demo.main
-  (:require [steroid.rn.core :as rn]))
+  (:require
+   [reagent.core :as reagent]
+   ;; ["react-native" :as rnn]
+   ;; ["react-native-reanimated" :default animated :refer (clockRunning EasingNode)]
+   [steroid.rn.core :as rn]))
+
+(def colors #{"green" "red" "blue" "orange" "magenta"})
+
+(defn
+  rand-text
+  [n]
+  [rn/text
+   {:key n :style {:background-color (rand-nth (into [] colors))}}
+   "fo"])
 
 (defn app-root []
-  [rn/text "Hello"])
+  [rn/view
+   (doall (map rand-text (range 100)))])
 
 (defn init []
   (rn/register-reload-comp "CljsRnShadowDemo" app-root))
+
+
+(comment
+  [rn/text
+   {:style {:color "green" :background-color "red"}} "Foo "
+   [foo]])
