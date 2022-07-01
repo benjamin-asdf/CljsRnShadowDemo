7464f2a8d44f81a398e143b236ab193d0b2980e7
Benj <Benjamin.Schwerdtner@gmail.com>
2022-07-01 13:56:07 +0200

    Messing around

--- src/org/sg/cljs_rn_shadow_demo/main.cljs
+++ src/org/sg/cljs_rn_shadow_demo/main.cljs
@@ -1,9 +1,11 @@
 (ns org.sg.cljs-rn-shadow-demo.main
   (:require
    [reagent.core :as reagent]
-   ;; ["react-native" :as rnn]
+   [reagent.react-native :as rn]
+   ["react-native" :as rnn]
    ;; ["react-native-reanimated" :default animated :refer (clockRunning EasingNode)]
-   [steroid.rn.core :as rn]))
+   ;; [steroid.rn.core :as rn]
+   ))
 
 (def colors #{"green" "red" "blue" "orange" "magenta"})
 
@@ -14,15 +16,40 @@
    {:key n :style {:background-color (rand-nth (into [] colors))}}
    "fo"])
 
+(defn
+  open-app-button
+  []
+  [rn/Button "foo"])
+
 (defn app-root []
   [rn/view
-   (doall (map rand-text (range 100)))])
+   (doall (map rand-text (range 50)))])
+
+(def dimensions (.-Dimensions ^js rnn))
+(def linking (.-Linking ^js rnn))
 
 (defn init []
-  (rn/register-reload-comp "CljsRnShadowDemo" app-root))
+  (rn/register-reload-comp "CljsRnShadowDemo" app-root)
+  (let [dimensions-change
+        (fn [evt]
+          (.openURL linking "https://stackoverflow.com/questions/58877079/how-to-open-one-app-from-another-app-react-native-android")
+          (prn "dimension change: " evt))]
+    (.addEventListener dimensions "change" dimensions-change)))
 
 
 (comment
-  [rn/text
-   {:style {:color "green" :background-color "red"}} "Foo "
-   [foo]])
+
+
+  (.openUrl linking "https://stackoverflow.com/questions/58877079/how-to-open-one-app-from-another-app-react-native-android")
+
+  ;; (js/J)
+  ;; (js/Dimensions)
+  ;; (js/ReactNative)
+  ;; (rnn/)
+  ;; js/ReactNative
+
+  ;; (js->clj dimensions)
+
+  ;; (rn/)
+
+  )
